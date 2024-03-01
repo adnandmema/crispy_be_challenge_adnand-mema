ffecbda82ff24b1b01a87f7843d2a8abf6c34977
Adnand Mema <adnand.dev@gmail.com>
2024-03-01 16:02:24 +0100

    Complete  Application layer

---
+++ ToDoApp.Application/Common/Behaviors/ExceptionLoggingBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Application.Common.Exceptions;
+
+namespace ToDoApp.Application.Common.Behaviors
+{
+    public class ExceptionLoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public ExceptionLoggingBehavior(ILogger<TRequest> logger)
+            => _logger = logger;
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (InputValidationException ive)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ive, "Validation error occurred in request '{RequestName}'\r\n\tRequestPayload: {@RequestPayload}\r\n\tErrors: {@Errors}.", requestName, request, ive.Errors);
+
+                throw;
+            }
+            catch (Exception e)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(e, "Exception occurred in request '{RequestName}'\r\n\tRequestPayload: {@RequestPayload}", requestName, request);
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ToDoApp.Application/Common/Behaviors/RequestValidationBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Application.Common.Exceptions;
+
+namespace ToDoApp.Application.Common.Behaviors
+{
+    public class RequestValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public RequestValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var results = await Task.WhenAll(_validators
+                    .Select(v => v.ValidateAsync(context)));
+
+                var failures = results
+                    .Where(r => !r.IsValid)
+                    .SelectMany(r => r.Errors)
+                    .ToList();
+
+                if (failures.Any())
+                {
+                    throw new InputValidationException(failures);
+                }
+            }
+
+            return await next();
+        }
+    }
+}
